e2694d7225db55eaf50a9d9c1df1c70eee97f876
Aaron Balentine <aaron@communityauctions.com>
2021-12-11 16:39:49 -0600

    Corrected typo

--- app/Http/Controllers/TMDBController.php
+++ app/Http/Controllers/TMDBController.php
@@ -7,7 +7,7 @@ use App\Custom\tmdb;
 
 class TMDBController extends Controller
 {
-    //Recieve incoming request and send to TMDB class for processing, return result to frontend
+    //Receive incoming request and send to TMDB class for processing, return result to frontend
     function send_request(Request $req){
 
         $tmdb = new tmdb();
